e3c294ee86b25eaf2190bafb66e0394fcd9e1113
Ezequiel <ingepasillasg@gmail.com>
2026-06-29 09:47:32 -0600

    feat: cors

--- index.js
+++ index.js
@@ -7,25 +7,34 @@ const app = express();
 
 // Node Server
 const server = require('http').createServer(app);
-module.exports.io = require('socket.io')(server);
+
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "*", // Permite que Flutter se conecte desde cualquier IP
+        methods: ["GET", "POST"]
+    }
+});
+
+module.exports.io = io;
+
 require('./sockets/socket');
 
 
 
 
 // Path público
-const publicPath = path.resolve( __dirname, 'public' );
-app.use( express.static( publicPath ) );
+const publicPath = path.resolve(__dirname, 'public');
+app.use(express.static(publicPath));
 
 
 
 
 
-server.listen( process.env.PORT, ( err ) => {
+server.listen(process.env.PORT, (err) => {
 
-    if ( err ) throw new Error(err);
+    if (err) throw new Error(err);
 
-    console.log('Servidor corriendo en puerto', process.env.PORT );
+    console.log('Servidor corriendo en puerto', process.env.PORT);
 
 });
 
